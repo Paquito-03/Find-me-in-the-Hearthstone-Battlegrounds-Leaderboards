6f2b78f17c187e75fca3845660d0968bb368c332
LucasPaquet <113984975+LucasPaquet@users.noreply.github.com>
2023-01-29 21:21:11 +0100

    Added possibility to choose region and season

--- README.md
+++ README.md
@@ -6,13 +6,7 @@ A small project to finding you in the Hearthstone Battlegrounds leaderboards. Th
 
 ## How to use
 
-Download the last release, run it, it will ask you the username are you looking for (upper and lower case are important) then press enter. After put the rating of the player, press enter again and the program will research the player
-
-**Exemples**
-
-![exemples](https://user-images.githubusercontent.com/110813707/187700724-1892d5a8-40ef-4676-9a8a-3be87a7a5195.png)
-
----
+Clone the project, install package write below and run the program. It will ask your four things : Name of the player, his rating, which season and which region. After that, the program will tell you your rank and on witch page you are
 
 ## Library to install
 
@@ -20,9 +14,20 @@ Download the last release, run it, it will ask you the username are you looking 
 `pip install requests`
 `pip install bs4`
 
+**Exemples**
+
+![exemples](https://user-images.githubusercontent.com/110813707/187700724-1892d5a8-40ef-4676-9a8a-3be87a7a5195.png)
+
+## Issues
+
+* Sometime, the leaderboard will load only the top 200 of BG, so the number of pages will not be right and the program can't find player under top 200
+* During the search, the page may not be completely loaded and therefore crash the program which will not be able to load the information correctly
+* If you make a mistake on the username or the rating, the program will looping infinty so you can just close the window
+
+---
+
 ## Disclaimer
 
-* At this moment, it work only in Europe
 * It is the [dichotomous algorithm](https://en.wikipedia.org/wiki/Dichotomic_search) searching the usersame (it is low iteration algorithm). But the leaderboard server is low so at every iteration, it takes some seconds to get the respond of the server, it is why it is slow
-* If you make a mistake on the username or the rating, the program will looping infinty so you can just close the window
-* I'm a beginner, so the code is'nt perfect (and the english too)
+* I can't release a .exe because all antivirus will block the program
+* I'm a beginner, so the code isn't perfect (and the english too)
--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup as soup
 
-url = 'https://hearthstone.blizzard.com/fr-fr/api/community/leaderboardsData?region=EU&leaderboardId=battlegrounds&page=1&seasonId=7'  # open a request for taking the max pages
+playerToFind = input("What player do you search : ")
+playerToFindRating = int(input("How many rating : "))
+seasonReseach = int(input("What season are you looking for : "))
+regionResearch = int(input("Which region are you looking for (US = 1, AP = 2, EU = 3) : "))
+
+seasonReseach -= 1  # seasonId starting to 0, so season 9 have the 8 id
+
+# replace number enter by user by the right word for the url
+if regionResearch == 1:
+    regionResearch = "US"
+elif regionResearch == 2:
+    regionResearch = "AP"
+elif regionResearch == 3:
+    regionResearch = "EU"
+
+url = 'https://hearthstone.blizzard.com/fr-fr/api/community/leaderboardsData?region=' + str(regionResearch) + '&leaderboardId=battlegrounds&page=10&seasonId=' + str(seasonReseach)  # open a request for taking the max pages
 req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})  # to prove we are not a robot
 
 webpage = urlopen(req).read()  # read the page
@@ -9,26 +24,21 @@ page_soup = soup(webpage, "html.parser")  # make it readable
 textFind = str(page_soup)  # transform the page for use fonction like .find()
 pageMaxFind = textFind.find("totalPages")  # find the position in the string (textFind)
 
-playerToFind = input("What player to you search : ")
-playerToFindRating = int(input("How many cote he have : "))
-
 x = 0  # for the while
 maxPage = ""  # be sure it is empty
 while textFind [pageMaxFind + 12 + int(
         x)] in "0123456789":  # loop for collect number of page : it is reading 12 letter after the position of totalPages in the request and loop when it is a figure. it avoid to make mistake for number because we don't known how many figure the number has
     maxPage = maxPage + textFind [pageMaxFind + 12 + int(x)]
     x = x + 1
-
 maxPage = int(maxPage)  # convertion into a int
 minPage = 1  # the first page
 i = int((maxPage + minPage) / 2)  # for dichotomous algorithm
 goAHead = True  # when he found page with the same rating, it search one + one, if he doesnt find the player, it goes into the 1 - 1
 
 while True:  # it is horrible, i know
-    url = 'https://hearthstone.blizzard.com/fr-fr/api/community/leaderboardsData?region=EU&leaderboardId=battlegrounds&page=' + str(
-        i) + '&seasonId=7'  # url with the page number of i
+    url = 'https://hearthstone.blizzard.com/fr-fr/api/community/leaderboardsData?region=' + str(regionResearch) + '&leaderboardId=battlegrounds&page=' + str(
+        i) + '&seasonId=' + str(seasonReseach)  # url with the page number of i
     req = Request(url, headers={'User-Agent': 'Mozilla/5.0'})
-
     webpage = urlopen(req).read()
     page_soup = soup(webpage, "html.parser")
     textFind = str(page_soup)
@@ -49,9 +59,10 @@ while True:  # it is horrible, i know
             displayRank = displayRank + textFind [
                 posPlayer + len(playerToFind) + len(str(playerToFindRating)) + 21 + int(x)]
             x = x + 1
-        print("We find " + playerToFind + " at page " + str(i) + ", rank : " + displayRank)  # print the Name, page where is it and its rank
+        print("We find " + playerToFind + " at page " + str(
+            i) + ", rank : " + displayRank)  # print the Name, page where is it and its rank
         break  # exit this loop
-    else:  # if the player is'nt find
+    else:  # if the player isn't find
         if playerToFindRating > int(iRating):  # if the player has more rating then the page
             maxPage = int(i)
             i = int((maxPage + minPage) / 2)
@@ -65,6 +76,11 @@ while True:  # it is horrible, i know
                 i = i + 1
             else:
                 i = i - 1
-        print("Searching page " + str(i) + " (best rating on this page : " + iRating + ")")  # for see what page he is looking for
+        print("Searching page " + str(
+            i) + " (best rating on this page : " + iRating + ")")  # for see what page he is looking for
 
-input("Press enter to exit...")
+# small condition to exit the program with only pressing enter
+x = input("Hit Enter to Exit")
+if not x:
+    print("Exiting the Program.")
+    exit()
